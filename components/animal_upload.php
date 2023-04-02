0ab503381ea737a663a72a735c976bae2785b00e
stefan44atvie <107038049+stefan44atvie@users.noreply.github.com>
2023-04-02 16:55:52 +0200

    new codes

--- components/animal_upload.php
+++ components/animal_upload.php
@@ -2,7 +2,7 @@
 function file_upload($picture)
 {
     $result = new stdClass(); //this object will carry status from file upload
-    $result->fileName = 'avatar.jpg';
+    $result->fileName = 'animal.jpg';
     $result->error = 1; //it could also be a boolean true/false
     //collect data from object $picture
     $fileName = $picture["name"];
@@ -10,8 +10,9 @@ function file_upload($picture)
     $fileTmpName = $picture["tmp_name"];
     $fileError = $picture["error"];
     $fileSize = $picture["size"];
+    $test1 ="test";
     $fileExtension = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
-    $filesAllowed = ["png", "jpg", "jpeg"];
+    $filesAllowed = ["pdf"];
     if ($fileError == 4) {
         $result->ErrorMessage = "No picture was chosen. It can always be updated later.";
         return $result;
@@ -20,18 +21,19 @@ function file_upload($picture)
             if ($fileError === 0) {
                 if ($fileSize < 5000000) { //500kb this number is in bytes
                     //it gives a file name based microseconds
-                    $fileNewName = uniqid('') . "." . $fileExtension; // 1233343434.jpg i.e
+                    $fileNewName = "picture" . "." . $fileExtension; // 1233343434.jpg i.e
                     $destination = "./pictures/animals/$fileNewName";
                     if (move_uploaded_file($fileTmpName, $destination)) {
                         $result->error = 0;
                         $result->fileName = $fileNewName;
                         return $result;
+                        
                     } else {
                         $result->ErrorMessage = "<br>There was an error uploading this file.";
                         return $result;
                     }
                 } else {
-                    $result->ErrorMessage = "<br>This picture is bigger than the allowed 500Kb. <br> Please choose a smaller one and Update your profile.";
+                    $result->ErrorMessage = "<br>This picture is bigger than the allowed 2MB. <br> Please choose a smaller one and Update your profile.";
                     return $result;
                 }
             } else {
